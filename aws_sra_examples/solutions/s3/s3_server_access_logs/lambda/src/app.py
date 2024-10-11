07dca873b7aed3e90a50d133e99b1686bef41e20
emmanuel <notifyemmanuel@gmail.com>
2024-10-11 13:07:32 -0500

    Regional bucket filtering

--- aws_sra_examples/solutions/s3/s3_server_access_logs/lambda/src/app.py
+++ aws_sra_examples/solutions/s3/s3_server_access_logs/lambda/src/app.py
@@ -12,11 +12,55 @@ import boto3
 import json
 
 s3 = boto3.client('s3')
-cloudtrail = boto3.client('cloudtrail')
 
 def lambda_handler(event, context):
     
+    bucket_name = event['detail']['requestParameters']['bucketName']
+    aws_account_id = event['account']
+    region = event['region']
+
+    # Check if the bucket has the "sra-server-access-logging-enabled" tag
+    try:
+        response = s3.get_bucket_tagging(Bucket=bucket_name)
+        tags = response['TagSet']
+        logging_enabled = any(tag['Key'] == 'sra-server-access-logging-enabled' and tag['Value'] == 'true' for tag in tags)
+    except s3.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchTagSet':
+            # If the bucket has no tags, logging is disabled
+            logging_enabled = False
+        else:
+            raise e
+        
+    # Bucket Tag filtering for logging
+    if logging_enabled:
+        # Enable s3 access logging
+        try:
+            s3.put_bucket_logging(
+                Bucket=bucket_name,
+                BucketLoggingStatus={
+                    'LoggingEnabled': {
+                        'TargetBucket': f"sra-server-access-log-{aws_account_id}-{region}-region",
+                        'TargetPrefix': f"{aws_account_id}/{region}/{bucket_name}/"
+                    }
+                }
+            )
+            print(f"Server access logging enabled for bucket {bucket_name} in {region}")
+        except s3.exceptions.ClientError as e:
+            print(f"Error enabling server access logging for bucket {bucket_name}: {e}")
+    
+    else:
+        # Disable server access logging
+        try:
+            s3.put_bucket_logging(
+                Bucket=bucket_name,
+                BucketLoggingStatus={}
+            )
+            print(f"Server access logging disabled for bucket {bucket_name}")
+        except s3.exceptions.ClientError as e:
+            print(f"Error disabling server access logging for bucket {bucket_name}: {e}")
+
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Lambda filtered the buckets successfully!')
     }
